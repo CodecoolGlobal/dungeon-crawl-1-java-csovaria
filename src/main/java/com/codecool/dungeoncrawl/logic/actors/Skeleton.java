b44aba8d2f2809f4119d23f6ca98879bbb088905
M9Bene <bene9marci@gmail.com>
2022-08-25 12:32:28 +0200

    Add: Skeleton damage

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Skeleton.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Skeleton.java
@@ -2,6 +2,7 @@ package com.codecool.dungeoncrawl.logic.actors;
 
 import com.codecool.dungeoncrawl.logic.Cell;
 
+import java.util.List;
 import java.util.Random;
 
 public class Skeleton extends Actor {
@@ -15,28 +16,39 @@ public class Skeleton extends Actor {
 
     @Override
     public void monsterMove() {
+        List<Cell> myNearbyCells = getNearbyCells(cell);
+        boolean didAttack = false;
+        for(Cell nearbyCell: myNearbyCells){
+            if(nearbyCell.getActor() instanceof Player){
+                int playersHealth = nearbyCell.getActor().getHealth();
+                nearbyCell.getActor().setHealth(playersHealth - damage);
+                didAttack = true;
+            }
+        }
+        if(!didAttack){
+            int randomDirection = new Random().nextInt(4);
+            Cell nextCell= cell.getNeighbor(0, 1);
+            switch (randomDirection) {
+                case 0:
+                    nextCell = cell.getNeighbor(0, 1);
+                    break;
+                case 1:
+                    nextCell = cell.getNeighbor(0, -1);
+                    break;
+                case 2:
+                    nextCell = cell.getNeighbor(1, 0);
+                    break;
+                case 3:
+                    nextCell = cell.getNeighbor(-1, 0);
+                    break;
+            }
+            if (nextCell.validatePlayerMove()) {
+                cell.setActor(null);
+                nextCell.setActor(this);
+                cell = nextCell;
+            }
+        }
 
-        int randomDirection = new Random().nextInt(4);
-        Cell nextCell= cell.getNeighbor(0, 1);
-        switch (randomDirection) {
-            case 0:
-                nextCell = cell.getNeighbor(0, 1);
-                break;
-            case 1:
-                nextCell = cell.getNeighbor(0, -1);
-                break;
-            case 2:
-                nextCell = cell.getNeighbor(1, 0);
-                break;
-            case 3:
-                nextCell = cell.getNeighbor(-1, 0);
-                break;
-        }
-        if (nextCell.validatePlayerMove()) {
-            cell.setActor(null);
-            nextCell.setActor(this);
-            cell = nextCell;
-        }
     }
 
 
